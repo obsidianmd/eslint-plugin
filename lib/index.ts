7c197074c11b6ff382a84e8028febd579a4130a0
Johannes Theiner <j.theiner@live.de>
2025-06-02 11:42:29 +0200

    prepare for first alpha npm release

--- lib/index.ts
+++ lib/index.ts
@@ -96,36 +96,6 @@ export = {
                 "obsidian/settings-tab": "error",
                 "obsidian/hardcoded-config-path": "error",
                 "obsidian/vault-iterate": "error",
-
-                "json-schema-validator/no-invalid": [
-                    "error", {
-                        "schemas": [
-                            {
-                                "fileMatch": ["manifest.json"],
-                                "schema": {
-                                    "$schema": "https://json-schema.org/draft/2020-12/schema",
-                                    "$id": "https://example.com/product.schema.json",
-                                    "title": "Product",
-                                    "description": "A product from Acme's catalog",
-                                    "type": "object",
-                                    "properties": {
-                                        "productId": {
-                                            "description": "The unique identifier for a product",
-                                            "type": "integer"
-                                        },
-                                        "productName": {"description": "Name of the product", "type": "string"},
-                                        "price": {
-                                            "description": "The price of the product",
-                                            "type": "number",
-                                            "exclusiveMinimum": 0
-                                        }
-                                    },
-                                    "required": ["productId", "productName", "price"]
-                                }
-                            }
-                        ]
-                    }
-                ]
             }
         }
     }
