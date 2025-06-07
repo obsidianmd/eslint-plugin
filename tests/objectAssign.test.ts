e91895efffe322c0e0ce4609969180d46e0fbfb8
saberzero1 <github@emilebangma.com>
2025-06-07 19:44:35 +0200

    Additional test cases to verify potential 'object-assign' issue

--- tests/objectAssign.test.ts
+++ tests/objectAssign.test.ts
@@ -4,10 +4,27 @@ import objectAssignRule from "../lib/rules/objectAssign.js";
 const ruleTester = new RuleTester();
 
 ruleTester.run("object-assign", objectAssignRule, {
-	valid: [{ code: "Object.assign({}, {foo: 1}, {bar: 2});" }],
+	valid: [
+		{ code: "Object.assign({}, {foo: 1}, {bar: 2});" },
+		{
+			code: "this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());",
+		},
+	],
 	invalid: [
 		{
 			code: "Object.assign(defaultConfig, config);",
+			errors: [{ messageId: "twoArgumentsDefault" }],
+		},
+		{
+			code: `
+				class MyPlugin {
+					settings: any;
+
+					async loadSettings() {
+						this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData());
+					}
+				}
+			`,
 			errors: [{ messageId: "twoArgumentsDefault" }],
 		},
 	],
