57e37a98c6971b5498409fac7177967ae0115a54
Johannes Theiner <j.theiner@live.de>
2025-10-23 12:02:22 +0200

    Refactor configuration to have separate configs for js and ts files

--- lib/index.ts
+++ lib/index.ts
@@ -68,162 +68,148 @@ const plugin: ESLint.Plugin = {
 	} as any,
 };
 
-const recommendedRulesConfig = {
-	plugins: ["obsidianmd"], // Helps tools associate rules with the plugin
-	rules: {
-		"obsidianmd/commands/no-command-in-command-id": "error",
-		"obsidianmd/commands/no-command-in-command-name": "error",
-		"obsidianmd/commands/no-default-hotkeys": "error",
-		"obsidianmd/commands/no-plugin-id-in-command-id": "error",
-		"obsidianmd/commands/no-plugin-name-in-command-name": "error",
-		"obsidianmd/settings-tab/no-manual-html-headings": "error",
-		"obsidianmd/settings-tab/no-problematic-settings-headings": "error",
-		"obsidianmd/vault/iterate": "error",
-		"obsidianmd/detach-leaves": "error",
-		"obsidianmd/hardcoded-config-path": "error",
-		"obsidianmd/no-forbidden-elements": "error",
-		"obsidianmd/no-plugin-as-component": "error",
-		"obsidianmd/no-sample-code": "error",
-		"obsidianmd/no-tfile-tfolder-cast": "error",
-		"obsidianmd/no-view-references-in-plugin": "error",
-		"obsidianmd/no-static-styles-assignment": "error",
-		"obsidianmd/object-assign": "error",
-		"obsidianmd/platform": "error",
-		"obsidianmd/prefer-file-manager-trash-file": "warn",
-		"obsidianmd/prefer-abstract-input-suggest": "error",
-		"obsidianmd/regex-lookbehind": "error",
-		"obsidianmd/sample-names": "error",
-		"obsidianmd/validate-manifest": "error",
-    	"obsidianmd/ui/sentence-case": ["warn", { enforceCamelCaseLower: true }],
-	} as const,
-};
+const recommendedPluginRulesConfig = {
+	"obsidianmd/commands/no-command-in-command-id": "error",
+	"obsidianmd/commands/no-command-in-command-name": "error",
+	"obsidianmd/commands/no-default-hotkeys": "error",
+	"obsidianmd/commands/no-plugin-id-in-command-id": "error",
+	"obsidianmd/commands/no-plugin-name-in-command-name": "error",
+	"obsidianmd/settings-tab/no-manual-html-headings": "error",
+	"obsidianmd/settings-tab/no-problematic-settings-headings": "error",
+	"obsidianmd/vault/iterate": "error",
+	"obsidianmd/detach-leaves": "error",
+	"obsidianmd/hardcoded-config-path": "error",
+	"obsidianmd/no-forbidden-elements": "error",
+	"obsidianmd/no-plugin-as-component": "error",
+	"obsidianmd/no-sample-code": "error",
+	"obsidianmd/no-tfile-tfolder-cast": "error",
+	"obsidianmd/no-view-references-in-plugin": "error",
+	"obsidianmd/no-static-styles-assignment": "error",
+	"obsidianmd/object-assign": "error",
+	"obsidianmd/platform": "error",
+	"obsidianmd/prefer-file-manager-trash-file": "warn",
+	"obsidianmd/prefer-abstract-input-suggest": "error",
+	"obsidianmd/regex-lookbehind": "error",
+	"obsidianmd/sample-names": "error",
+	"obsidianmd/validate-manifest": "error",
+    "obsidianmd/ui/sentence-case": ["error", { enforceCamelCaseLower: true }],
+}
+
+const flatRecommendedGeneralRules = {
+	"no-unused-vars": "off",
+	"no-prototype-bultins": "off",
+	"no-self-compare": "warn",
+	"no-eval": "error",
+	"no-implied-eval": "error",
+	"prefer-const": "off",
+	"no-implicit-globals": "error",
+	"no-console": ["error", { allow: ["warn", "error", "debug"] }],
+	"no-restricted-globals": [
+		"error",
+		{
+			name: "app",
+			message:
+				"Avoid using the global app object. Instead use the reference provided by your plugin instance.",
+		},
+		"warn",
+		{
+			name: "fetch",
+			message:
+				"Use the built-in `requestUrl` function instead of `fetch` for network requests in Obsidian.",
+		},
+		{
+			name: "localStorage",
+			message: "Prefer `App#saveLocalStorage` / `App#loadLocalStorage` functions to write / read localStorage data that's unique to a vault."
+		}
+	],
+	"no-restricted-imports": [
+		"error",
+		{
+			name: "axios",
+			message:
+				"Use the built-in `requestUrl` function instead of `axios`.",
+		},
+		{
+			name: "superagent",
+			message:
+				"Use the built-in `requestUrl` function instead of `superagent`.",
+		},
+		{
+			name: "got",
+			message:
+				"Use the built-in `requestUrl` function instead of `got`.",
+		},
+		{
+			name: "node-fetch",
+			message:
+				"Use the built-in `requestUrl` function instead of `node-fetch`.",
+		},
+		{
+			name: "moment",
+			message:
+				"The 'moment' package is bundled with Obsidian. Please import it from 'obsidian' instead.",
+		},
+	],
+	"no-alert": "error",
+	"no-undef": "error",
+	"@typescript-eslint/ban-ts-comment": "off",
+	"@typescript-eslint/no-deprecated": "error",
+	"@typescript-eslint/no-unused-vars": ["warn", { args: "none" }],
+	"@typescript-eslint/no-explicit-any": [
+		"error",
+		{ fixToUnknown: true },
+	],
+	"@microsoft/sdl/no-document-write": "error",
+	"@microsoft/sdl/no-inner-html": "error",
+	"import/no-nodejs-modules":
+		manifest && manifest.isDesktopOnly ? "off" : "error",
+	"import/no-extraneous-dependencies": "error",
+}
 
 const flatRecommendedConfig = [
 	js.configs.recommended,
-	...tseslint.configs.recommendedTypeChecked,
 	{
 		plugins: {
 			import: importPlugin,
 			"@microsoft/sdl": sdl,
 			obsidianmd: plugin,
 		},
-		files: ["**/*.ts", "**/*.js"],
+		files: ['**/*.js', "**/*.jsx"],
+		extends: [tseslint.configs.recommended],
 		rules: {
-			"no-unused-vars": "off",
-			"no-prototype-bultins": "off",
-			"no-self-compare": "warn",
-			"no-eval": "error",
-			"no-implied-eval": "error",
-			"prefer-const": "off",
-			"no-implicit-globals": "error",
-			"no-console": ["warn", { allow: ["warn", "error", "debug"] }],
-			"no-restricted-globals": [
-				"error",
-				{
-					name: "app",
-					message:
-						"Avoid using the global app object. Instead use the reference provided by your plugin instance.",
-				},
-				"warn",
-				{
-					name: "fetch",
-					message:
-						"Use the built-in `requestUrl` function instead of `fetch` for network requests in Obsidian.",
-				},
-				{
-					name: "localStorage",
-					message: "Prefer `App#saveLocalStorage` / `App#loadLocalStorage` functions to write / read localStorage data that's unique to a vault."
-				}
-			],
-			"no-restricted-imports": [
-				"error",
-				{
-					name: "axios",
-					message:
-						"Use the built-in `requestUrl` function instead of `axios`.",
-				},
-				{
-					name: "superagent",
-					message:
-						"Use the built-in `requestUrl` function instead of `superagent`.",
-				},
-				{
-					name: "got",
-					message:
-						"Use the built-in `requestUrl` function instead of `got`.",
-				},
-				{
-					name: "node-fetch",
-					message:
-						"Use the built-in `requestUrl` function instead of `node-fetch`.",
-				},
-				{
-					name: "moment",
-					message:
-						"The 'moment' package is bundled with Obsidian. Please import it from 'obsidian' instead.",
-				},
-			],
-			"no-alert": "error",
-			"no-undef": "error",
-			"@typescript-eslint/ban-ts-comment": "off",
-			"@typescript-eslint/no-deprecated": "error",
-			"@typescript-eslint/no-unused-vars": ["warn", { args: "none" }],
-			"@typescript-eslint/no-explicit-any": [
-				"error",
-				{ fixToUnknown: true },
-			],
-			"@microsoft/sdl/no-document-write": "error",
-			"@microsoft/sdl/no-inner-html": "error",
-			"import/no-nodejs-modules":
-				manifest && manifest.isDesktopOnly ? "off" : "error",
-			"import/no-extraneous-dependencies": "error",
-
-			...recommendedRulesConfig.rules,
-		},
+			...flatRecommendedGeneralRules,
+			...recommendedPluginRulesConfig
+		}
 	},
 	{
-		// manifest.json validation
 		plugins: {
-			"json-schema-validator": jsonSchemaValidator,
+			import: importPlugin,
+			"@microsoft/sdl": sdl,
 			obsidianmd: plugin,
 		},
-		files: ["manifest.json"],
+		files: ['**/*.ts', "**/*.tsx"],
+		extends: [tseslint.configs.recommendedTypeChecked],
 		rules: {
-			"@typescript-eslint/no-unused-expressions": "off",
-			"json-schema-validator/no-invalid": "error",
-			"obsidianmd/validate-manifest": "error",
+			...flatRecommendedGeneralRules,
+			...recommendedPluginRulesConfig
 		},
-	},
-	{
-		// LICENSE validation
-		plugins: {
-			obsidianmd: plugin,
-		},
-		parserOptions: {
-			parser: PlainTextParser,
-			extraFileExtensions: [""],
-		},
-		files: ["LICENSE"],
-		rules: {
-			"obsidianmd/validate-license": "error",
-		},
-	},
-] as any;
+	}
+	]
 
 const hybridRecommendedConfig = {
 	// Properties for eslint-doc-generator to read
-	...recommendedRulesConfig,
+	...recommendedPluginRulesConfig,
 
 	// Make the object iterable for the ESLint 9 runtime
 	[Symbol.iterator]: function* () {
-		yield* flatRecommendedConfig;
+		yield* flatRecommendedConfig
 	},
 };
 
 plugin.configs = {
 	recommended: hybridRecommendedConfig,
 	recommendedWithLocalesEn: {
-		...recommendedRulesConfig,
+		...recommendedPluginRulesConfig,
 		[Symbol.iterator]: function* () {
 			yield* flatRecommendedConfig;
 			// JSON English locales
